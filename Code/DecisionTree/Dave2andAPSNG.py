62ccc9a3c709dc9e722e6519a38f17f1a1e1316d
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2024-09-29 10:29:34 -0400

    Update Dave2andAPSNG.py

--- Code/DecisionTree/Dave2andAPSNG.py
+++ Code/DecisionTree/Dave2andAPSNG.py
@@ -361,10 +361,16 @@ for hh in range(1, 10):
   fpaths = []
   for run in range(20):
 
-    classifier = DecisionTreeClassifier(max_depth = 5, class_weight= 'balanced')
+    dtmodel = BayesSearchCV(DecisionTreeClassifier(max_depth = 5, class_weight = 'balanced'), {'prune_size': [0.1, 0.8], 'k': [1, 3]}, optimizer_kwargs = {'acq_func': 'EI'})
+
+    try:
+      dtmodel.fit(X, y)
+      params = dtmodel.best_params_
+      classifier = DecisionTreeClassifier(**params)
+    except:
+      classifier = DecisionTreeClassifier(max_depth = 5, class_weight = 'balanced')
 
     classifier.fit(X, y)
-
 
 
     pickle_file = datachosen+"_decision_tree_dataset"+str(hh)+"_run"+str(run)+".pkl"
