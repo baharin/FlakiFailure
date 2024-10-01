b0b050e09090d2db30a799b9fa699deef1debe4f
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2024-10-01 17:36:50 -0400

    Update ConsistencyCheckingRouter.py

--- Evaluation/Code/RQ2/ConsistencyCheckingRouter.py
+++ Evaluation/Code/RQ2/ConsistencyCheckingRouter.py
@@ -19,7 +19,7 @@ class Vertex:
             return self.weight > other.weight
         if self.degree != other.degree:
             return self.degree > other.degree
-        return self.part == 'A' and other.part == 'B' #part A is pass, part B is fail
+        return self.part == 'A' and other.part == 'B'  # part A is pass, part B is fail
 
 def remove_vertices(graph, part_A, part_B, weights):
     degrees = {v: len(neighbors) for v, neighbors in graph.items()}
@@ -32,7 +32,6 @@ def remove_vertices(graph, part_A, part_B, weights):
         vertices.sort()
 
     removed_vertices = set()
-
     removed_vertices_withpassfail = set()
 
     while vertices:
@@ -43,18 +42,16 @@ def remove_vertices(graph, part_A, part_B, weights):
         removed_vertices.add(vertex.id)
         removed_vertices_withpassfail.add(vertex)
 
-        while len(list(graph[vertex.id])) > 0:  #iterate over neighbors of vertex
+        while len(list(graph[vertex.id])) > 0:  # iterate over neighbors of vertex
             neighbor = list(graph[vertex.id])[0]
-
             graph[neighbor].remove(vertex.id)
             if neighbor not in removed_vertices:
                 degrees[neighbor] -= 1
                 vertices = []
-                for v in graph: #reconstruct the heap
-                  part = 'A' if v in part_A else 'B'
-                  heapq.heappush(vertices, Vertex(v, degrees[v], weights[v], part))
-                  vertices.sort()
-
+                for v in graph:  # reconstruct the heap
+                    part = 'A' if v in part_A else 'B'
+                    heapq.heappush(vertices, Vertex(v, degrees[v], weights[v], part))
+                    vertices.sort()
 
                 heapq.heappop(vertices)
 
@@ -65,58 +62,35 @@ def remove_vertices(graph, part_A, part_B, weights):
         if all(not neighbors for neighbors in graph.values()):
             break
 
-
-
-    return sorted(removed_vertices), sorted(removed_vertices_withpassfail, key = lambda v: v.id)
+    return sorted(removed_vertices), sorted(removed_vertices_withpassfail, key=lambda v: v.id)
 
 def ExtractVars(rulee):
-
-  pattern = r'ARG\d+'
-
-  return re.findall(pattern, rulee)
-
+    pattern = r'ARG\d+'
+    return re.findall(pattern, rulee)
 
 def ExtractValue(rulee):
-
-  pattern = r'\d+\.\d+'
-
-  return re.findall(pattern, rulee)
-
+    pattern = r'\d+\.\d+'
+    return re.findall(pattern, rulee)
 
 def CreateFormulaNTSS(vars, floatt, rulee):
-
-  if len(vars) == 1:
-    if rulee.find('greater') != -1:  
-
-      var1 = Int(vars[0])
-
-      formula = And(var1 > int(float(floatt[0])))
-
-    elif rulee.find('less') != -1:
-      var1 = Int(vars[0])
-
-      formula = And(var1 < int(float(floatt[0])))
-
-
-  else:
-
-    variables = [Int(x) for x in vars]
-
-    sum_all = sum(variables)
-
-    if rulee.find('greater') != -1:
-
-      formula = And(sum_all > int(float(floatt[0])))
-
+    if len(vars) == 1:
+        if rulee.find('greater') != -1:
+            var1 = Int(vars[0])
+            formula = And(var1 > int(float(floatt[0])))
+        elif rulee.find('less') != -1:
+            var1 = Int(vars[0])
+            formula = And(var1 < int(float(floatt[0])))
     else:
+        variables = [Int(x) for x in vars]
+        sum_all = sum(variables)
+        if rulee.find('greater') != -1:
+            formula = And(sum_all > int(float(floatt[0])))
+        else:
+            formula = And(sum_all < int(float(floatt[0])))
 
-      formula = And(sum_all < int(float(floatt[0])))
-
-
-  return formula
+    return formula
 
 def remove_common_elements(list1, list2):
-
     set1 = set(list1)
     set2 = set(list2)
     common_elements = set1 & set2
@@ -126,131 +100,47 @@ def remove_common_elements(list1, list2):
 
     return list1, list2
 
+def process_assertions(fail_assertions, pass_assertions):
+    results = []
+    
+    inconsistencies = defaultdict(list)
 
-results = []
+    # Solver for Z3
+    s = Solver()
 
-for lll in range(1, 10):
+    for A_1 in pass_assertions:
+        vars = ExtractVars(A_1)
+        floatt = ExtractValue(A_1)
+        A = CreateFormulaNTSS(vars, floatt, A_1)
 
-  res = pd.DataFrame(columns = ['Run1', 'Run1Prob', 'Run2', 'Run2Prob', 'Run3', 'Run3Prob', 'Run4', 'Run4Prob', 'Run5', 'Run5Prob', 'Run6', 'Run6Prob', 'Run7', 'Run7Prob', 'Run8', 'Run8Prob', 'Run9', 'Run9Prob', 'Run10', 'Run10Prob', 'Run11', 'Run11Prob', 'Run12', 'Run12Prob', 'Run13', 'Run13Prob', 'Run14', 'Run14Prob', 'Run15', 'Run15Prob', 'Run16', 'Run16Prob', 'Run17', 'Run17Prob', 'Run18', 'Run18Prob', 'Run19', 'Run19Prob', 'Run20', 'Run20Prob'])
+        for B_1 in fail_assertions:
+            s = Solver()  # Reset solver for each pair
+            vars = ExtractVars(B_1)
+            floatt = ExtractValue(B_1)
+            B = CreateFormulaNTSS(vars, floatt, B_1)
 
-  fail_assertions1 = pd.read_excel('...\\path_to_file\\pure fail class rules - GP2.xlsx', sheet_name='dataset'+str(lll))
-  pass_assertions1 = pd.read_excel('...\\path_to_file\\pure pass class rules - GP2.xlsx', sheet_name='dataset' + str(lll))
+            s.add(A, B)
 
-  cols = ['Run'+str(i) for i in range(1, 21)]
+            if s.check() == sat:
+                print(f"Inconsistent pair found: {A} and {B}")
+                inconsistencies[A_1].append(B_1)
+                inconsistencies[B_1].append(A_1)
+            else:
+                print(f"Consistent pair found: {A} and {B}")
 
-  for kk in range(3):
-
-    indexx = res.shape[0]
-
-    for col in cols:
-
-      fail_assertions = list(set(fail_assertions1.loc[360 + kk*20:379 + kk*20, col].dropna().values.tolist()))
-
-      pass_assertions = list(set(pass_assertions1.loc[360 + kk*20:379 + kk*20, col].dropna().values.tolist()))
-
-      i = 0
-      j = 0
-
-      s = Solver()
-
-
-      inconsistencies = defaultdict(list) 
-
-      while i < len(pass_assertions):
-
-          A_1 = pass_assertions[i]
-
-          vars = ExtractVars(A_1)
-          floatt = ExtractValue(A_1)
-          A = CreateFormulaNTSS(vars, floatt, A_1)
-
-          j = 0
-
-          while j < len(fail_assertions):
-
-              s = Solver()
-              B_1 = fail_assertions[j]
-
-              vars = ExtractVars(B_1)
-              floatt = ExtractValue(B_1)
-              B = CreateFormulaNTSS(vars, floatt, B_1)
-
-              s.add(A, B)
-  
-              if s.check() == sat:
-
-                  print(f"Inconsistent pair found: {A} and {B}")
-
-                  inconsistencies[A_1].append(B_1)
-
-                  inconsistencies[B_1].append(A_1)
-
-              else:
-                  print(f"Consistent pair found: {A} and {B}")
-
-              j += 1
-
-          i += 1
-
-
-      weights = {}
-
-      for i in range(len(pass_assertions)):
-
-
-        rule = pass_assertions[i]
-
+    weights = {}
+    for rule in pass_assertions + fail_assertions:
         vars = ExtractVars(rule)
+        weights[rule] = 1 / len(vars)
 
+    removed_vertices, removed_vertices_withpassfail = remove_vertices(inconsistencies, pass_assertions, fail_assertions, weights)
 
-        weights[rule] = 1/len(vars)
-
-      for i in range(len(fail_assertions)):
-
-        rule = fail_assertions[i]
-
-        vars = ExtractVars(rule)
-
-        weights[rule] = 1/len(vars)
-
-      ######################
-      removed_vertices, removed_vertices_withpassfail = remove_vertices(inconsistencies, pass_assertions, fail_assertions, weights)
-
-
-      for item in removed_vertices_withpassfail:
-        if item.part == 'A': #from pass
-          pass_assertions.remove(item.id)
+    for item in removed_vertices_withpassfail:
+        if item.part == 'A':  # from pass
+            pass_assertions.remove(item.id)
         else:
-          fail_assertions.remove(item.id)
+            fail_assertions.remove(item.id)
 
-      for hh in range(len(fail_assertions)):
+    results.append((fail_assertions.copy(), pass_assertions.copy()))
 
-        res.loc[hh + indexx, col] = fail_assertions[hh]
-
-      res.loc[len(fail_assertions) + indexx, col] = '[]'
-
-      for hh in range(len(pass_assertions)):
-
-        res.loc[len(fail_assertions) + 1 + hh + indexx, col] = pass_assertions[hh]
-
-      for gg in range(30 - (len(fail_assertions) + 1 + len(pass_assertions))):
-         
-         res.loc[len(fail_assertions) + len(pass_assertions) + 1 + gg + indexx, col] = '###'
-      
-
-
-  results.append(res)
-
-  import os
-
-  file_exists = os.path.isfile('finalresultsntss.xlsx')
-
-  if file_exists:
-    with pd.ExcelWriter('finalresultsntss.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
-      for i, df in enumerate(results):
-        df.to_excel(writer, sheet_name=f'dataset{i+1}', index=False)
-
-  else:
-    with pd.ExcelWriter('finalresultsntss.xlsx', engine='openpyxl', mode = 'w')  as writer:
-      for i, df in enumerate(results):
-        df.to_excel(writer, sheet_name=f'dataset{i+1}', index=False)
+    return results
