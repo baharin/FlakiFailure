0779d3580ee51c0b9141105c93a3b1c7fe36cb4d
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2024-10-01 16:13:07 -0400

    Update CoveredPointsDT-APSNG-Dave2.py

--- Evaluation/Code/RQ2/CoveredPointsDT-APSNG-Dave2.py
+++ Evaluation/Code/RQ2/CoveredPointsDT-APSNG-Dave2.py
@@ -380,36 +380,6 @@ def Preprocess(dataa):
   return dataa
 
 
-def RemoveFlakyTests(data, rep, features, label):
-
-  i = 0
-  while i < len(data.index):
-
-    flag = False
-
-    data.reset_index(drop=True, inplace=True)
-
-    for j in range(rep):
-
-      # if data.loc[i + j, ["Weather", "Maxspeed", "MAX_ANGLE"]].to_list() == data.loc[i, ["Weather", "Maxspeed", "MAX_ANGLE"]].to_list() and data.loc[i, 'TestOutcome'] != data.loc[i+j, 'TestOutcome']:
-        if data.loc[i + j, features].to_list() == data.loc[i, features].to_list() and data.loc[i, label] != data.loc[i+j, label]:
-
-          flag = True
-          break
-
-    if flag == True: #the test is flaky
-
-      # print('yes')
-
-      data = data.drop([i + j for j in range(rep)])
-
-      i = 0
-
-    else:
-      i = i + rep
-
-  return data
-
 def FindGroups(df):
 
   groups = []
@@ -616,60 +586,6 @@ def PreprocessRule(rulee):
 
   return listt
 
-def HandleFlakyTests(data, rep, features, label, model):
-
-  i = 0
-  k = 0
-
-  featurescopy = features
-  featurescopy.append(label)
-
-  newdata = pd.DataFrame(columns = featurescopy)
-
-  while i < len(data.index):
-
-    countfails = 0
-    countpass = 0
-
-    newdata.loc[k, features] = data.loc[i, features]
-
-    for j in range(rep):
-
-      if data.loc[i + j, label] == 1 or data.loc[i + j , label]  == 'PASS':
-
-        countpass = countpass + 1
-
-      elif data.loc[i + j, label] == 0 or data.loc[i + j, label] == 'FAIL':
-
-        countfails = countfails + 1
-
-    if countpass > countfails:
-
-      if 'town' in model:
-
-        newdata.loc[k, label] = 1
-      else:
-
-        newdata.loc[k, label] = 'PASS'
-
-    else:
-      if 'town' in model:
-
-        newdata.loc[k, label] = 0
-
-      else:
-
-        newdata.loc[k, label] = 'FAIL'
-
-    i = i + rep
-
-    k = k + 1
-
-  return newdata
-
-
-
-
 def PreprocessWithoutRepetition(data, rep, model):
 
 
@@ -832,7 +748,7 @@ import ast
 
 model = 'Dave2' #change to APSNG for APSNG
 
-thetas = [0 , 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
+thetas = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
 
 for hh in range(1, 10):
 
@@ -844,7 +760,6 @@ for hh in range(1, 10):
     cols = ['Weather', 'Maxspeed' , 'MAX_ANGLE']
     collabel = 'TestOutcome'
 
-    testset = HandleFlakyTests(testset, 10, cols, collabel, model)
     testset = Preprocess(testset)
     res = testset
 
