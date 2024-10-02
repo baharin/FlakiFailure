3d5c02a5bb4f348b8c1107d5843cfa837a79ef45
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2024-10-02 16:00:21 -0400

    Update executors.py

--- BeamNG/APSNG-Dave2/executors.py
+++ BeamNG/APSNG-Dave2/executors.py
@@ -1,4 +1,4 @@
-#This code adds additional functionalities to the beamng_executer code from https://github.com/sbft-cps-tool-competition/cps-tool-competition/tree/main
+#This code adds additional functionalities to the executer code from https://github.com/sbft-cps-tool-competition/cps-tool-competition/tree/main
 import logging as log
 import random
 import time
