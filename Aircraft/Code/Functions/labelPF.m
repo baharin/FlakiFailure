668c4fde49ccca9e13aa8075354d757fa997e03e
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2025-03-13 11:00:42 -0400

    Update labelPF.m

--- Aircraft/Code/Functions/labelPF.m
+++ Aircraft/Code/Functions/labelPF.m
@@ -1,23 +1,11 @@
 function label = labelPF(PopObj,high,req)
     %Label data point into B or NB using PopObj
-
-%For certain requirements only
-    if (strcmp(req,'R1b')) || (strcmp(req,'R1e')) || (strcmp(req,'R2a')) || (strcmp(req,'R2b')) || (strcmp(req,'R7'))
-        if (PopObj >= high)
-            label = 1; %B
-            disp("pass");
-        else
-            label = 0; %NB
-            disp("fail");
-        end
+    if (PopObj >= 0)
+        label = 1; %B
+        disp("pass");
     else
-        if (PopObj >= 0)
-            label = 1; %B
-            disp("pass");
-        else
-            label = 0; %NB
-            disp("fail");
-        end
+        label = 0; %NB
+        disp("fail");
     end
-
+    
 end
