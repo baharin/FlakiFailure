1431d0da50a3003bf46fa1f6029e3a178cd3e12f
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2025-03-17 12:42:09 -0400

    Update Router.py

--- Code/Ensemble/Router.py
+++ Code/Ensemble/Router.py
@@ -183,10 +183,7 @@ def remove_common_elements(list1, list2):
 
 def PreprocessDTDR(r):
 
-  print('now i am r ', r)
-
-  # r_list = list(r)
-
+  
   for i in range(len(r)):
 
     r_list = list(r[i])
@@ -218,20 +215,23 @@ def GetFailAndPassAssertions(assertions, theta):
     i = 0
     while i < len(assertions.index) and assertions.loc[i, col] != '[]' and type(assertions.loc[i, col]) != float:
 
-      r = PreprocessDTDR(ast.literal_eval(assertions.loc[i, col]))
+      if assertions.loc[i, col+'Prob'] >= theta:
+            r = PreprocessDTDR(ast.literal_eval(assertions.loc[i, col]))
       
 
-      Failassertions.append(r)
+            Failassertions.append(r)
 
       i = i + 1
 
     i = i + 1
     while i < len(assertions.index) and assertions.loc[i, col] != '[]' and type(assertions.loc[i, col]) != float:
 
-      r = PreprocessDTDR(ast.literal_eval(assertions.loc[i, col]))
+      if assertions.loc[i, col+'Prob'] >= theta:
+
+          r = PreprocessDTDR(ast.literal_eval(assertions.loc[i, col]))
 
 
-      Passassertions.append(r)
+          Passassertions.append(r)
 
       i = i + 1
 
@@ -319,213 +319,212 @@ def ExtractValueDTDR(rulee):
 
   return floatss
 
-
-for kk in range(1, 10):
-
-  gpfail = pd.read_excel('..\\pure fail class rules - GP2.xlsx', sheet_name='dataset'+str(kk))[360:420].reset_index(drop=True)
-
-  gppass = pd.read_excel('..\\pure pass class rules - GP2.xlsx', sheet_name='dataset'+str(kk))[360:420].reset_index(drop=True)
-
-  dtind = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[120:139].reset_index(drop=True)
-
-  dtsum = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[140:159].reset_index(drop=True)
-
-  drind = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[280:299].reset_index(drop=True)
-
-  drsum = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[300:319].reset_index(drop=True)
-
-  cols = ['Run'+str(i) for i in range(1, 21)]
-
-  gpasserts = []
-
-  for col in cols:
-
-    Failassertions = []
-    Passassertions = []
-
-    i = 0
-    while i < len(gpfail.index) and gpfail.loc[i, col] != '[]' and type(gpfail.loc[i, col]) != float:
-
-      Failassertions.append(gpfail.loc[i, col])
-
-      i = i + 1
-
-    i = 0
-    while  i < len(gppass.index) and gppass.loc[i, col] != '[]' and type(gppass.loc[i, col]) != float:
-
-      Passassertions.append(gppass.loc[i, col])
-
-      i = i + 1
-
-    gpasserts.append([Failassertions, Passassertions])
-
-  dtassertind = GetFailAndPassAssertions(dtind, 0)
-  dtassertsum = GetFailAndPassAssertions(dtsum, 0)
-  drassertind = GetFailAndPassAssertions(drind, 0)
-  drassertsum = GetFailAndPassAssertions(drsum, 0)
-
-
-  ensemble = dtassertind
-
-  for i in range(len(ensemble)): #combine everything
-
-    for j in range(len(ensemble[i])):
-
-      # print(drassert[i][j])
-      # print(gpasserts[i][j])
-
-      ensemble[i][j].extend(dtassertsum[i][j])
-      ensemble[i][j].extend(drassertind[i][j])
-      ensemble[i][j].extend(drassertsum[i][j])
-      ensemble[i][j].extend(gpasserts[i][j])
-
-
-
-
-  res = pd.DataFrame(columns = ['Run1', 'Run1Prob', 'Run2', 'Run2Prob', 'Run3', 'Run3Prob', 'Run4', 'Run4Prob', 'Run5', 'Run5Prob', 'Run6', 'Run6Prob', 'Run7', 'Run7Prob', 'Run8', 'Run8Prob', 'Run9', 'Run9Prob', 'Run10', 'Run10Prob', 'Run11', 'Run11Prob', 'Run12', 'Run12Prob', 'Run13', 'Run13Prob', 'Run14', 'Run14Prob', 'Run15', 'Run15Prob', 'Run16', 'Run16Prob', 'Run17', 'Run17Prob', 'Run18', 'Run18Prob', 'Run19', 'Run19Prob', 'Run20', 'Run20Prob'])
-
-
-  cols = ['Run'+str(i) for i in range(1, 21)]
-
-  for ind, col in enumerate(cols):
-
-
-    fail_assertions = ensemble[ind][0]
-
-    pass_assertions = ensemble[ind][1]
-
+for theta in [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]:
+    for kk in range(0, 10):
     
-    i = 0
-    j = 0
+      gpfail = pd.read_excel('..\\pure fail class rules - GP2.xlsx', sheet_name='dataset'+str(kk))[360:420].reset_index(drop=True)
+    
+      gppass = pd.read_excel('..\\pure pass class rules - GP2.xlsx', sheet_name='dataset'+str(kk))[360:420].reset_index(drop=True)
+    
+      dtind = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[120:139].reset_index(drop=True)
+    
+      dtsum = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[140:159].reset_index(drop=True)
+    
+      drind = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[280:299].reset_index(drop=True)
+    
+      drsum = pd.read_excel('..\\DT and DR Assertions.xlsx', sheet_name='dataset'+str(kk))[300:319].reset_index(drop=True)
+    
+      cols = ['Run'+str(i) for i in range(1, 21)]
+    
+      gpasserts = []
+    
+      for col in cols:
+    
+        Failassertions = []
+        Passassertions = []
+    
+        i = 0
+        while i < len(gpfail.index) and gpfail.loc[i, col] != '[]' and type(gpfail.loc[i, col]) != float:
 
-    s = Solver()
+          if  gpfail.loc[i, col+'Prob'] >= theta:
+              Failassertions.append(gpfail.loc[i, col])
+    
+          i = i + 1
+    
+        i = 0
+        while  i < len(gppass.index) and gppass.loc[i, col] != '[]' and type(gppass.loc[i, col]) != float:
 
-    inconsistencies = defaultdict(list)  # dictionary of inconsistencies (the graph)
-
-
-    while i < len(pass_assertions):
-
-        A_1 = str(pass_assertions[i])
-
-        if type(pass_assertions[i]) == str: #NTSS
-
-          
-          vars = ExtractVars(A_1)
-          floatt = ExtractValue(A_1)
-          A = CreateFormulaNTSS(vars, floatt, A_1)
-
-
-        else: #DT / DR
-
-          vars = ExtractVarsDTDR(pass_assertions[i])
-          floatt = ExtractValueDTDR(pass_assertions[i])
-          A = CreateFormulaNTSS(vars, floatt, A_1)
-
-        # A = CreateFormula(A_1)
-
-        j = 0
-
-        while j < len(fail_assertions):
-
-            s = Solver()
-            B_1 = str(fail_assertions[j])
-
-            if type(fail_assertions[j]) == str: #GP
-
-              
-              vars = ExtractVars(B_1)
-              floatt = ExtractValue(B_1)
-              B = CreateFormulaNTSS(vars, floatt, B_1)
-
-            else: #DT / DR
-
-              vars = ExtractVarsDTDR(fail_assertions[j])
-              floatt = ExtractValueDTDR(fail_assertions[j])
-              B = CreateFormulaNTSS(vars, floatt, B_1)
-
-            s.add(A, B)
-
-            if s.check() == sat:
-                
-                inconsistencies[A_1].append(B_1)
+          if gppass.loc[i, col+'Prob'] >= theta:  
+              Passassertions.append(gppass.loc[i, col])
+    
+          i = i + 1
+    
+        gpasserts.append([Failassertions, Passassertions])
+    
+      dtassertind = GetFailAndPassAssertions(dtind, theta)
+      dtassertsum = GetFailAndPassAssertions(dtsum, theta)
+      drassertind = GetFailAndPassAssertions(drind, theta)
+      drassertsum = GetFailAndPassAssertions(drsum, theta)
+    
+    
+      ensemble = dtassertind
+    
+      for i in range(len(ensemble)): #combine everything
+    
+        for j in range(len(ensemble[i])):
+    
+          ensemble[i][j].extend(dtassertsum[i][j])
+          ensemble[i][j].extend(drassertind[i][j])
+          ensemble[i][j].extend(drassertsum[i][j])
+          ensemble[i][j].extend(gpasserts[i][j])
+    
+    
+    
+    
+      res = pd.DataFrame(columns = ['Run1', 'Run1Prob', 'Run2', 'Run2Prob', 'Run3', 'Run3Prob', 'Run4', 'Run4Prob', 'Run5', 'Run5Prob', 'Run6', 'Run6Prob', 'Run7', 'Run7Prob', 'Run8', 'Run8Prob', 'Run9', 'Run9Prob', 'Run10', 'Run10Prob', 'Run11', 'Run11Prob', 'Run12', 'Run12Prob', 'Run13', 'Run13Prob', 'Run14', 'Run14Prob', 'Run15', 'Run15Prob', 'Run16', 'Run16Prob', 'Run17', 'Run17Prob', 'Run18', 'Run18Prob', 'Run19', 'Run19Prob', 'Run20', 'Run20Prob'])
+    
+    
+      cols = ['Run'+str(i) for i in range(1, 21)]
+    
+      for ind, col in enumerate(cols):
+    
+    
+        fail_assertions = ensemble[ind][0]
+    
+        pass_assertions = ensemble[ind][1]
+    
         
-                inconsistencies[B_1].append(A_1)
-
-            else:
-                print(f"Consistent pair found: {A} and {B}")
-
-            j += 1
-
-        i += 1
-
-
-    weights = {}
-
-    for i in range(len(pass_assertions)):
-
-      rule = pass_assertions[i]
-
-      if type(pass_assertions[i]) == str: #NTSS
-
-        weights[pass_assertions[i]] = 1/len(ExtractVars(rule))
-
-      else: #DT / DR
-
-        weights[str(pass_assertions[i])] = 1/len(ExtractVarsDTDR(rule))
-
-    for i in range(len(fail_assertions)):
-
-      rule = fail_assertions[i]
-
-      if type(fail_assertions[i]) == str: #NTSS
-
-        weights[fail_assertions[i]] = 1/len(ExtractVars(rule))
-
-        # print(fail_assertions[i], type(fail_assertions[i]))
-
-      else: #DT / DR
-
-        weights[str(fail_assertions[i])] = 1/len(ExtractVarsDTDR(rule))
-
-
-    ######################
+        i = 0
+        j = 0
     
-    removed_vertices, removed_vertices_withpassfail = remove_vertices(inconsistencies, pass_assertions, fail_assertions, weights)
+        s = Solver()
     
-    for item in removed_vertices_withpassfail:
-      
-      if item.part == 'A': #from pass
-        try:
-          pass_assertions.remove(ast.literal_eval(item.id))
+        inconsistencies = defaultdict(list)  # dictionary of inconsistencies (the graph)
+    
+    
+        while i < len(pass_assertions):
+    
+            A_1 = str(pass_assertions[i])
+    
+            if type(pass_assertions[i]) == str: #NTSS
+    
+              
+              vars = ExtractVars(A_1)
+              floatt = ExtractValue(A_1)
+              A = CreateFormulaNTSS(vars, floatt, A_1)
+    
+    
+            else: #DT / DR
+    
+              vars = ExtractVarsDTDR(pass_assertions[i])
+              floatt = ExtractValueDTDR(pass_assertions[i])
+              A = CreateFormulaNTSS(vars, floatt, A_1)
+    
+            # A = CreateFormula(A_1)
+    
+            j = 0
+    
+            while j < len(fail_assertions):
+    
+                s = Solver()
+                B_1 = str(fail_assertions[j])
+    
+                if type(fail_assertions[j]) == str: #GP
+    
+                  
+                  vars = ExtractVars(B_1)
+                  floatt = ExtractValue(B_1)
+                  B = CreateFormulaNTSS(vars, floatt, B_1)
+    
+                else: #DT / DR
+    
+                  vars = ExtractVarsDTDR(fail_assertions[j])
+                  floatt = ExtractValueDTDR(fail_assertions[j])
+                  B = CreateFormulaNTSS(vars, floatt, B_1)
+    
+                s.add(A, B)
+    
+                if s.check() == sat:
+                    
+                    inconsistencies[A_1].append(B_1)
+            
+                    inconsistencies[B_1].append(A_1)
+    
+                else:
+                    print(f"Consistent pair found: {A} and {B}")
+    
+                j += 1
+    
+            i += 1
+    
+    
+        weights = {}
+    
+        for i in range(len(pass_assertions)):
+    
+          rule = pass_assertions[i]
+    
+          if type(pass_assertions[i]) == str: #NTSS
+    
+            weights[pass_assertions[i]] = 1/len(ExtractVars(rule))
+    
+          else: #DT / DR
+    
+            weights[str(pass_assertions[i])] = 1/len(ExtractVarsDTDR(rule))
+    
+        for i in range(len(fail_assertions)):
+    
+          rule = fail_assertions[i]
+    
+          if type(fail_assertions[i]) == str: #NTSS
+    
+            weights[fail_assertions[i]] = 1/len(ExtractVars(rule))
+    
+            # print(fail_assertions[i], type(fail_assertions[i]))
+    
+          else: #DT / DR
+    
+            weights[str(fail_assertions[i])] = 1/len(ExtractVarsDTDR(rule))
+    
+    
+        ######################
+        
+        removed_vertices, removed_vertices_withpassfail = remove_vertices(inconsistencies, pass_assertions, fail_assertions, weights)
+        
+        for item in removed_vertices_withpassfail:
           
-        except:
-          pass_assertions.remove(item.id)
-
+          if item.part == 'A': #from pass
+            try:
+              pass_assertions.remove(ast.literal_eval(item.id))
+              
+            except:
+              pass_assertions.remove(item.id)
+    
+          else:
+            try:
+              fail_assertions.remove(ast.literal_eval(item.id))
+              
+            except:
+    
+              fail_assertions.remove(item.id)
+    
+        for hh in range(len(fail_assertions)):
+    
+          res.loc[hh, col] = fail_assertions[hh]
+    
+        res.loc[len(fail_assertions), col] = '[]'
+    
+        for hh in range(len(pass_assertions)):
+    
+          res.loc[len(fail_assertions) + 1 + hh, col] = pass_assertions[hh]
+    
+      file_exists = os.path.isfile('ensembleNTSS.xlsx')
+    
+      if file_exists:
+          with pd.ExcelWriter('ensembleRouter'+theta+'.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+              res.to_excel(writer, sheet_name='dataset'+str(kk), index=False)
+    
       else:
-        try:
-          fail_assertions.remove(ast.literal_eval(item.id))
-          
-        except:
-
-          fail_assertions.remove(item.id)
-
-    for hh in range(len(fail_assertions)):
-
-      res.loc[hh, col] = fail_assertions[hh]
-
-    res.loc[len(fail_assertions), col] = '[]'
-
-    for hh in range(len(pass_assertions)):
-
-      res.loc[len(fail_assertions) + 1 + hh, col] = pass_assertions[hh]
-
-  file_exists = os.path.isfile('ensembleNTSS.xlsx')
-
-  if file_exists:
-      with pd.ExcelWriter('ensembleRouter.xlsx', engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
-          res.to_excel(writer, sheet_name='dataset'+str(kk), index=False)
-
-  else:
-      with pd.ExcelWriter('ensembleRouter.xlsx', engine='openpyxl', mode = 'w')  as writer:
-          res.to_excel(writer, sheet_name='dataset'+str(kk), index=False)  
+          with pd.ExcelWriter('ensembleRouter'+theta+'.xlsx', engine='openpyxl', mode = 'w')  as writer:
+              res.to_excel(writer, sheet_name='dataset'+str(kk), index=False)  
 
